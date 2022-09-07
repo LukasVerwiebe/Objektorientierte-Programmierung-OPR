22c8b8de396dc43d839af9d9998187880ce2412d
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-09-07 04:52:19 +0200

    Update WoerterStreamTest.java

--- O_Aufgabe_15_Wörterstream/Woerterstream/src/woerterstream/WoerterStreamTest.java
+++ O_Aufgabe_15_Wörterstream/Woerterstream/src/woerterstream/WoerterStreamTest.java
@@ -7,4 +7,11 @@ package woerterstream;
  */
 public class WoerterStreamTest {
     
+     public static void main(String[] args) {
+        
+        Woerterstream stream = new Woerterstream();
+        
+        stream.gibStream().limit(100).forEach(System.out::println);
+        System.out.println("ST " + stream.gibWerte());        
+    }    
 }
